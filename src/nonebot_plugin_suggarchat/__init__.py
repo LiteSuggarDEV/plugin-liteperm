e728c473a956ec23d1fbdbe8e3e65ec3c8dfdc2f
JohnRichard4096 <windowserror@163.com>
2025-04-10 22:46:10 +0800

    First commit

---
+++ src/nonebot_plugin_suggarchat/__init__.py
@@ -0,0 +1,13 @@
+from nonebot.plugin import PluginMetadata, require
+
+require("nonebot_plugin_localstore")
+
+
+__plugin_meta__ = PluginMetadata(
+    name="LitePerm 权限管理插件",
+    description="基于权限节点/权限组/特殊权限的权限管理插件。",
+    usage="https://github.com/LiteSuggarDEV/plugin-liteperm/blob/main/README.md",
+    homepage="https://github.com/LiteSuggarDEV/plugin-liteperm/",
+    type="application",
+    supported_adapters={"~onebot.v11"},
+)
